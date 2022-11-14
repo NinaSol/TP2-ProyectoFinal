718fcc30fc8e4401cd72a9ee7655e58ec2a04385
NinaSol <ninamarotta18@gmail.com>
2022-11-13 21:09:54 -0600

    fix server.js

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/cineort/peliculas", new RouterPeliculas().start());
 app.use("/cineort", new FuncionesRouter().start());
 app.use("/cineort/usuarios", new UsuariosRouter().start());
