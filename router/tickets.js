27351e132980b2e26c60034954dd954099279334
NinaSol <ninamarotta18@gmail.com>
2022-11-16 15:58:40 -0600

    arreglos varios en tickets

--- router/tickets.js
+++ router/tickets.js
@@ -1,24 +1,27 @@
-import express from 'express'
-import ControllerTicket from '../controller/tickets.js'
-
+import express from "express";
+import ControllerTicket from "../controller/tickets.js";
 
 export class TicketRouter {
-    constructor() {
-        this.router = express.Router()
-        this.ticketController = new ControllerTicket()
-    }
+  constructor() {
+    this.router = express.Router();
+    this.ticketController = new ControllerTicket();
+  }
 
-    start() {
-        this.router.get('/:id?', this.ticketController.obtenerTicket)
-        this.router.get('/', this.ticketController.obtenerTicket)
-        this.router.get('/ultimoTicket', this.ticketController.obtenerUltimoTicket)
-        this.router.get('/diaMayorConsumo', this.ticketController.obtenerTicketMayorConsumo)
-        this.router.get('/diaMenorConsumo', this.ticketController.obtenerTicketMenorConsumo)
-        this.router.post('/', this.ticketController.crearTicket)
-        //this.router.put('/:id', this.ticketController.actualizarTicket)
-        //this.router.delete('/:id', this.ticketController.eliminarTicket)
+  start() {
+    this.router.get("/ultimoTicket", this.ticketController.obtenerUltimoTicket);
+    this.router.get(
+      "/mayorConsumo",
+      this.ticketController.obtenerTicketMayorConsumo
+    );
+    this.router.get(
+      "/menorConsumo",
+      this.ticketController.obtenerTicketMenorConsumo
+    );
+    this.router.get("/:id?", this.ticketController.obtenerTicket);
+    this.router.post("/", this.ticketController.crearTicket);
+    //this.router.put('/:id', this.ticketController.actualizarTicket)
+    //this.router.delete('/:id', this.ticketController.eliminarTicket)
 
-        return this.router
-    }
+    return this.router;
+  }
 }
-
