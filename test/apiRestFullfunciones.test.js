714f65e995bc09c2874f99db0b868cd45185887f
Martina Pereyra <1martinapereyra@gmail.com>
2022-11-23 00:20:36 -0300

    agrego test de funciones

--- test/apiRestFullfunciones.test.js
+++ test/apiRestFullfunciones.test.js
@@ -1,18 +1,15 @@
 import supertest from 'supertest';
-const request = supertest('http://localhost:8082')
+const request = supertest('http://localhost:8080')
 
-//import * as chai from 'chai';
 import {expect} from 'chai'
-import { assert } from 'chai';  // Using Assert style
-import { should } from 'chai';  // Using Should style
-should();  // Modifies `Object.prototype`
-
-
+import generador from '../generador/funciones.js'
+import { should } from 'chai';  
+should();  
 
 describe('test api rest full', () => {
 
     describe('GET', () => {
-        it('debería retornar un status 200', async () => {
+        it('debería retornar un obtejo y status 200', async () => {
             let responseFunciones = await request.get('/cineort/funciones')
             expect(responseFunciones.status).to.eql(200)
             const funciones = responseFunciones.body
@@ -22,7 +19,7 @@ describe('test api rest full', () => {
     })
 
     describe('Update', () => {
-        it('debería retornar un status 200', async () => {
+        it('debería retornar un objeto y status 200', async () => {
             let responseFunciones = await request.get('/cineort/funciones/1')
             expect(responseFunciones.status).to.eql(200)
             const funciones = responseFunciones.body
@@ -32,7 +29,7 @@ describe('test api rest full', () => {
     })
 
     describe('Menor capacidad de funciones', () => {
-        it('debería retornar un numero', async () => {
+        it('debería retornar un numero y devolver estatus 200', async () => {
             let responseFunciones = await request.get('/cineort/funciones/menosVendida')
             expect(responseFunciones.status).to.eql(200)
             const funciones = responseFunciones.body
@@ -42,7 +39,7 @@ describe('test api rest full', () => {
     })
 
     describe('Calcular capacidad', () => {
-        it('debería retornar un numero', async () => {
+        it('debería retornar un numero y devolver status 200', async () => {
             let responseFunciones = await request.get('/cineort/funciones/calcularCapacidad/1')
             expect(responseFunciones.status).to.eql(200)
             const funciones = responseFunciones.body
@@ -52,22 +49,34 @@ describe('test api rest full', () => {
     })
 
 
-
-
     describe("POST /funciones", function () {
-        let funcion = {
-            idPelicula: 4,
-            capacidad: 15,
-            capacidadMax: 20,
-            fecha: "01-12-22"
-        }
-        it('Agregar una función', async function () {
+        let funcion =  generador.get()
+        it('Debería agregar una función con campos válidos', async function () {
           const response = await request.post("/cineort/funciones")
           .send(funcion)
           expect(response.status).to.eql(200);
           
-          const user = response.body
-          expect(user).to.include.keys('hola','capacidad', 'capacidadMax', 'fecha')
+          const funcionCreada = response.body
+          expect(funcion).to.include.keys('idPelicula','capacidad', 'capacidadMax', 'fecha')
+
+          expect(funcionCreada.idPelicula).to.eql(funcion.idPelicula)
+          expect(funcionCreada.capacidad).to.eql(funcion.capacidad)
+          expect(funcionCreada.capacidadMax).to.eql(funcion.capacidadMax)
+          expect(funcionCreada.fecha).to.eql(funcion.fecha)
+        });
+
+      });
+
+      describe("Get /obtenerFuncionesPorPelicula", function () {
+        it('Debería retornar status 200 y encotrar la función del Id de Pelicula pasado', async function () {
+          const response = await request.get("/cineort/funciones/obtenerPorPelicula/2")
+          expect(response.status).to.eql(200);
+          expect(response).to.be.an("object")
+          expect("Content-Type", /json/);
+
+          console.log(response)
+          expect(response.body).to.include.keys('idPelicula','capacidad', 'capacidadMax', 'fecha')
+
         });
 
       });
